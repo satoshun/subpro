8bacbee081a475fff41d4b1fd325b94b6962f306
SatoShun <shun.sato1@gmail.com>
2014-05-05 17:39:16 +0900

    refactoring: use constants values

--- setup.sh
+++ setup.sh
@@ -8,5 +8,8 @@ chmod 755 ${root}/subpro
 cp ./subpro_completion ${root}/__subpro_completion
 chmod 755 ${root}/__subpro_completion
 
+cp ./__subpro_variables ${root}/__subpro_variables
+chmod 755 ${root}/__subpro_variables
+
 mkdir -p ~/.subpro
 cp -r base ~/.subpro/
